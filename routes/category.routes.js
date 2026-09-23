4297e5fb867632c8ac67665168faa63bb9ecf963
Husna <husnaali8uni@gmail.com>
2026-09-23 22:16:17 +0300

    feat:make the category route only for admin

--- routes/category.routes.js
+++ routes/category.routes.js
@@ -1,16 +1,14 @@
 //this is for admin role
-const router = require("express").Router();
-const Category = require("../models/Category.js");
-
+const router = require("express").Router()
+const Category = require("../models/Category.js")
+const isAdmin = require("../middleware/is-admin.js")
 //adding categories
-router.get("/new", (req, res) => {
-
+router.get("/new", isAdmin, (req, res) => {
     res.render("category-new.ejs")
 
 })
 
-router.post("/", async (req, res) => {
-
+router.post("/", isAdmin, async (req, res) => {
     await Category.create({name: req.body.name})
 
     res.redirect("/products/new")

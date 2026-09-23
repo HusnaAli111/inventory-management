f9f2115bd4c1013edcb4bd360e5582b372f24511
Husna <husnaali8uni@gmail.com>
2026-09-23 22:28:08 +0300

    feat:admin can edit the category

--- routes/category.routes.js
+++ routes/category.routes.js
@@ -10,10 +10,33 @@ router.get("/new", isAdmin, (req, res) => {
 
 router.post("/", isAdmin, async (req, res) => {
     await Category.create({name: req.body.name})
-
     res.redirect("/products/new")
 
 })
+//this is for where the admin can see the categories that exist
+router.get("/", isAdmin, async (req, res) => {
+
+    const categories = await Category.find()
+    res.render("categories.ejs", {categories: categories})
+
+})
+//edit category for admin
+
+router.get("/:id/edit", isAdmin, async (req, res) => {
+
+    const category = await Category.findById(req.params.id)
+    res.render("category-edit.ejs", {category: category})
+
+})
+
+router.put("/:id", isAdmin, async (req, res) => {
+
+    await Category.findByIdAndUpdate(req.params.id, {name: req.body.name})
+    res.redirect("/categories")
+
+})
+
+
 
 
 module.exports = router;
